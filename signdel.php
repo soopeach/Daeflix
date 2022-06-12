70328d142fced46ed3d5790feede24d5f247ff35
soopeach <hsjeon584@gmail.com>
2022-06-13 01:38:35 +0900

    Choroe: 아주 사소한 오타 수정

--- signdel.php
+++ signdel.php
@@ -15,10 +15,14 @@ $sqlForLikeVideo = "delete from likeVideo where email = '$email'";
 session_destroy(); 
 if($conn->query($sqlForPayment)) {
 	if($conn->query($sql)) {
-    session_destroy();
-    echo "<script>alert('회원탈퇴가 성공적으로 진행되었습니다..');location.href='index.php'</script>";
+        if($conn->query($sqlForLikeVideo)){
+            session_destroy();
+            echo "<script>alert('회원탈퇴가 성공적으로 진행되었습니다.');location.href='index.php'</script>";
+        } else {
+            "<script>alert('찜목록 삭제중 오류가 발생하였습니다.');location.href='index.php'</script>";
+        }
     } else {
-        echo "<script>alert('결제 정보 삭제중 요류가 발생하였습니다.');location.href='index.php'</script>";
+        echo "<script>alert('결제 정보 삭제중 오류가 발생하였습니다.');location.href='index.php'</script>";
     }
    
 }
