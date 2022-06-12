2f1c745f788369adb31aa25384cb7514d9603459
soopeach <hsjeon584@gmail.com>
2022-06-12 21:05:47 +0900

    Feat: 검색기능 추가

--- liked.php
+++ liked.php
@@ -174,14 +174,16 @@
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="container-fluid">
-                        <div class="input-group">
-                            <input class="form-control" type="search" placeholder="검색할 영화의 제목을 입력해주세요" aria-label="Search">
-                            <button class="btn btn-primary" type="submit">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 20">
-                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                                </svg>
-                            </button>
-                        </div>
+                        <form action="search.php" method="post">
+                            <div class="input-group">
+                                <input class="form-control" type="search" placeholder="검색할 영화의 제목을 입력해주세요" aria-label="Search" name="search">
+                                <button class="btn btn-primary" type="submit">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 20">
+                                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                    </svg>
+                                </button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
--- search.php
+++ search.php
@@ -15,7 +15,15 @@
     <!--상단바-->
     <nav id="navbar" class="navbar navbar-expand-md fixed-top navbar-dark" style="background-color: #000000">
         <?php
-        if($logged){ // 로그인이 된 경우.
+            include_once("dbconn.php");
+            session_start();
+            $logged = false;
+            if (isset($_SESSION['email'])){ // 세션에 id 키가 정의되어 있으면
+                $email = $_SESSION['email'];
+                $nickName = $_SESSION['nickName'];
+                $logged = true;
+            }
+            $title = $_POST['search'];
         ?>
         <div class="container-fluid">
             <!--상단바 로고-->
@@ -30,7 +38,7 @@
                         <a class="nav-link active" aria-current="page">내가 찜한 목록</a>
                     </li>
                     <li class="nav-item pt-0 pb-0">
-                        <a class="nav-link" aria-current="page" role="button" href="liked.html" data-bs-toggle="tooltip" data-bs-placement="right" title="찜목록 전체 보기!">
+                        <a class="nav-link" aria-current="page" role="button" href="liked.php" data-bs-toggle="tooltip" data-bs-placement="right" title="찜목록 전체 보기!">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="20" fill="currentColor" class="bi bi-justify-left" viewBox="0 0 18 20" style="color:#eee;">
                                 <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
                             </svg>
@@ -41,7 +49,7 @@
                     <!--프로필 드롭다운-->
                     <li class="nav-item dropdow" data-bs-toggle="tooltip" data-bs-placement="bottom" title="누르면 열려요!">
                         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" data-bs-display="static" aria-expanded="false" style="color: #eee;">
-                            타디스블루 님의 프로필<!--여기에 닉네임 표시해야 함-->
+                            <?=$nickName?> 님의 프로필<!--여기에 닉네임 표시해야 함-->
                         </a>
                         <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink" id="dropmenu">
                             <li>
@@ -88,13 +96,41 @@
                 </form>   
             </div> 
         </div>
-        <?php } ?>
     </nav>
+    <?php
+        $sql = "select * from videoInfo where title like '%$title%'";
+        if($result = $conn -> query($sql)){
+    ?>
     <div class="container px-1" style="margin-top: 90px;">
+        <?php
+            while($row = $result->fetch_assoc()){
+        ?>
+        <h3 id="tab">
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-binoculars" viewBox="0 0 16 20" style="color:dodgerblue;">
+                <path d="M3 2.5A1.5 1.5 0 0 1 4.5 1h1A1.5 1.5 0 0 1 7 2.5V5h2V2.5A1.5 1.5 0 0 1 10.5 1h1A1.5 1.5 0 0 1 13 2.5v2.382a.5.5 0 0 0 .276.447l.895.447A1.5 1.5 0 0 1 15 7.118V14.5a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 14.5v-3a.5.5 0 0 1 .146-.354l.854-.853V9.5a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v.793l.854.853A.5.5 0 0 1 7 11.5v3A1.5 1.5 0 0 1 5.5 16h-3A1.5 1.5 0 0 1 1 14.5V7.118a1.5 1.5 0 0 1 .83-1.342l.894-.447A.5.5 0 0 0 3 4.882V2.5zM4.5 2a.5.5 0 0 0-.5.5V3h2v-.5a.5.5 0 0 0-.5-.5h-1zM6 4H4v.882a1.5 1.5 0 0 1-.83 1.342l-.894.447A.5.5 0 0 0 2 7.118V13h4v-1.293l-.854-.853A.5.5 0 0 1 5 10.5v-1A1.5 1.5 0 0 1 6.5 8h3A1.5 1.5 0 0 1 11 9.5v1a.5.5 0 0 1-.146.354l-.854.853V13h4V7.118a.5.5 0 0 0-.276-.447l-.895-.447A1.5 1.5 0 0 1 12 4.882V4h-2v1.5a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V4zm4-1h2v-.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5V3zm4 11h-4v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14zm-8 0H2v.5a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5V14z"/>
+            </svg>
+            <?=$row['title']?>
+        </h3>
         <div class="continer">
-            
+            <!--아마 이 부분을 반복시키면 될 듯??-->
+            <div class="row gx-5" >
+                <div class="col-lg-3 mt-3"> <!--사진이 들어갈 곳-->
+                    <div class="border-0">
+                        <a href=""><img src="<?=$row['preview']?>" id="gallery"></a>
+                    </div>
+                </div>
+                <div class="col-lg-9 mt-3 pb-3" id="specbox"> <!--제목과 줄거리가 들어갈 곳-->
+                    <h3 id="title" class="mt-3"><?=$row['title']?></h3>
+                    <p id="story"><?=$row['summary']?><br>
+                    </p>
+                </div>   
+            </div>
+        <?php }} else echo "<script>alert('검색된 영상이 없습니다!'); history.go(-1)</script>"; ?>
         </div>
     </div>
+    
+    <!--멋 진 공 백-->
+    <div class="container-fluid" id="void"></div>
     <script> // 툴팁을 이쁘게 불러오기 위한 자바스크립트
         var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
         var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
